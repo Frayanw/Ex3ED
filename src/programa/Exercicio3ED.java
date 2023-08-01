4307f926c020ab71e1746d22485ae25dd056bbdf
Frayan <frayanweyne1@gmail.com>
2023-08-01 13:09:32 -0300

    Exercício3ED

---
+++ src/programa/Exercicio3ED.java
@@ -0,0 +1,18 @@
+
+package programa;
+
+public class Exercicio3ED {
+
+    public static void main(String[] args) {
+        /*Faça um programa de consulta de telefones a partir de um nome informado como uma chave de
+        dados. O programa deve:
+        ? Conter uma classe Pessoa com os atributos nome, telefone e e-mail;
+        ? Permitir a entrada de nomes de pessoas com seus respectivos telefones e e-mails, sendo a
+        quantidade determinada pelo usuário, e armazená-los em um vetor de objetos pessoas;
+        ? Permitir ao usuário inserir qual o nome que ele deseja consultar o telefone. Após a consulta,
+        exiba o telefone da pessoa procurada. Informe também se o nome é inexistente no vetor de
+        pessoas.*/
+        
+    }
+    
+}
